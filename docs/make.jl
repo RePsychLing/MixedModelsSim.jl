575b62b954c3851c783bd60cc18a3f53b2e04539
Douglas Bates <dmbates@gmail.com>
2019-09-25 12:32:53 +0200

    Use Project.toml in subdirectory

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,13 @@ using Documenter, MixedModelsSim
 
 makedocs(;
     modules=[MixedModelsSim],
-    format=Documenter.HTML(),
+    format=Documenter.HTML(assets=String[]),
     pages=[
         "Home" => "index.md",
     ],
     repo="https://github.com/RePsychLing/MixedModelsSim.jl/blob/{commit}{path}#L{line}",
     sitename="MixedModelsSim.jl",
     authors="Phillip Alday, Douglas Bates, Shravan Vasishth, Reinhold Kliegl",
-    assets=String[],
 )
 
 deploydocs(;
