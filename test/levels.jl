c9b420655fe2549635eb2461ef6db2330eedb934
Douglas Bates <dmbates@gmail.com>
2019-09-25 18:36:29 +0200

    Update tests.

--- test/levels.jl
+++ test/levels.jl
@@ -1,19 +1,8 @@
 using MixedModelsSim, PooledArrays, Test
 
 @testset "levels" begin
-    levs = makelevels(["N","Y"], inner=2)
-    @test levs isa PooledVector
-    @test length(levs) == 4
-    @test length(unique(levs)) == 2
-    @test isa(first(levs), String)
-    @test first(levs) isa String
-    @test_throws MethodError makelevels("foo")
-    @test first(levs) == levs[2] == "N"
-    @test levs[3] == last(levs) == "Y"
-    nsubj = 20
-    subj = makelevels(nsubj, outer=2)
-    @test length(subj) == 2*nsubj
-    @test first(subj) == "S01"
-    @test subj[2] == "S02"
-    @test last(subj) == "S$nsubj"
+    levs = nlevels(100, 'k')
+    @test length(levs) == 100
+    @test length(unique(levs)) == 100
+    @test first(levs) == "k001"
 end
