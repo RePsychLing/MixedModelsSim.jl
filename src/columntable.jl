e4d123432ec20d7449353c6ca6d71c7717987e81
Douglas Bates <dmbates@gmail.com>
2019-09-26 15:25:23 +0200

    Add documentation and examples

--- src/columntable.jl
+++ src/columntable.jl
@@ -1,16 +1,12 @@
 """
-    nlevels(nlev, tag='S')
-
-Return a `Vector{String}` of `tag` followed by `1:nlev` padded with zeros
-"""
-nlevels(nlev, tag='S') = string.(tag, lpad.(1:nlev, ndigits(nlev), '0'))
-
-"""
     crossedDataFrame(namedtup)
 
 Return a `DataFrame` obtained by crossing factors given in `namedtup`.
 
 `namedtup` should be a named tuple of vectors of strings giving the levels.
+```@example
+crossedDataFrame((prec=["break","maintain"], load=["yes","no"]))
+```
 """
 function crossedDataFrame(namedtup)
     nms = keys(namedtup)
@@ -22,39 +18,57 @@ function crossedDataFrame(namedtup)
     DataFrame(NamedTuple{keys(ctbl)}(PooledArray.(values(ctbl))))
 end
 
-function itemsubjdf(nitem, nsubj, expfactors=())
-    df = crossedDataFrame((item=nlevels(nitem, 'I'), subj=nlevels(nsubj)))
-    if !isempty(expfactors)
-        exptbl = crossedDataFrame(expfactors)
-        ncond = size(exptbl, 1)
-        if !iszero(rem(nitem, ncond))
-            @warn "number of items, $nitem, is not a multiple of number of conditions, $ncond"
-        end
-        if !iszero(rem(nsubj, ncond))
-            @warn "number of subjects, $nsubj, is not a multiple of number of conditions, $ncond"
-        end
-        nrow = size(df, 1)
-        q, r = divrem(nrow, ncond)
-        if !iszero(r)
-            throw(ArgumentError("number of rows, $nrow, is not a multiple of number of conditions, $ncond"))
-        end
-                    # create a latin square design in the conditions by cyclical shifts
-        v = collect(1:ncond)
-        inds = typeof(v)[]
-        for _ in 1:q
-            append!(inds, v)
-            cyclicshift!(v)
-        end
-        df = hcat(df, exptbl[inds,!])
-    end
+"""
+    cyclicshift(v::AbstractVector, nrow)
+
+Return an `eltype(v)` matrix of size `nrow` by `length(v)` with each column consisting
+of `v` followed by a cyclic shift of `v` followed by ...
+
+The return value is used to counterbalance levels of conditions in [`withinitem`](@ref).
+```@example
+cyclicshift('a':'d', 8)
+```
+"""
+function cyclicshift(v::AbstractVector, nrow)
+    vlen = length(v)
+    [v[1 + (i + j) % vlen] for i in 0:(nrow - 1), j in 0:(vlen - 1)]
+end
+
+"""
+    itemsubjdf(nitem, nsubj, expfactors)
+
+Return a data frame with `nitem` items crossed with `nsubj` subjects and experimental factors
+given by `expfactors`.  The experimental factors are in a crossed, balanced configuration within
+subject and within item.
+"""
+function itemsubjdf(nitem, nsubj, expfactors)
+    expfacdf = crossedDataFrame(expfactors)
+    df = repeat(withinitem(nitem, expfacdf), outer=nsubj ÷ size(expfacdf,1))
+    df.subj = repeat(PooledArray(nlevels(nsubj)), inner=nitem)
     df
 end
 
-function cyclicshift!(v::AbstractVector)
-    tmp = first(v)
-    @inbounds for i in 2:length(v)
-        v[i-1] = v[i]
-    end
-    v[end] = tmp
-    v
+"""
+    nlevels(nlev, tag='S')
+
+Return a `Vector{String}` of `tag` followed by `1:nlev` padded with zeros
+```@example
+show(nlevels(10))
+```
+"""
+nlevels(nlev, tag='S') = string.(tag, lpad.(1:nlev, ndigits(nlev), '0'))
+
+"""
+    withinitem(nitem, df)
+
+Return a `DataFrame` of `item` with `nitem` levels and balanced columns of conditions from `df`
+"""
+function withinitem(nitem, df)
+    nrow = size(df, 1)
+    q, r = divrem(nitem, nrow)
+    iszero(r) || throw(ArgumentError("nitem = $nitem is not a multiple of nrow = $nrow"))
+    value = df[vec(cyclicshift(1:nrow, nitem)), :]
+    value.item = repeat(PooledArray(string.('I', lpad.(string.(1:nitem), ndigits(nitem), '0'))),
+        outer=nrow)
+    value
 end
