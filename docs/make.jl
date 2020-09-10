42384397c5f8adff4b4df8f8b03e97f771fa2ea7
Douglas Bates <dmbates@gmail.com>
2020-09-10 16:07:18 -0500

    Move makedocs call to docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,14 @@
 using Documenter, MixedModelsSim
 
+makedocs(;
+    modules = [MixedModelsSim],
+    format = Documenter.HTML(assets = String[]),
+    pages = ["Home" => "index.md"],
+    repo = "https://github.com/RePsychLing/MixedModelsSim.jl/blob/{commit}{path}#L{line}",
+    sitename = "MixedModelsSim.jl",
+    authors = "Phillip Alday, Douglas Bates, Lisa DeBruine, Reinhold Kliegl",
+    source = joinpath("..", "docs", "src"),
+    build = joinpath("..", "docs", "build"),
+)
+
 deploydocs(; repo = "github.com/RePsychLing/MixedModelsSim.jl", push_preview = true)
