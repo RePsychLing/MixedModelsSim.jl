4a716f6f5ba4ab879553325c29643f27cef78f37
Phillip Alday <me@phillipalday.com>
2021-02-19 10:48:51 +0100

    new power values for updated sim

--- test/sim.jl
+++ test/sim.jl
@@ -9,14 +9,16 @@ subj_btwn = Dict(:age => ["O", "Y"],
 				:pet => ["cat", "dog"])
 item_btwn = Dict(:cond => ["A", "B"])
 both_win = Dict(:time => ["morning", "evening"])
-dat = simdat_crossed(StableRNG(42), subj_n, item_n, 
-					 subj_btwn = subj_btwn, 
-					 item_btwn = item_btwn, 
+dat = simdat_crossed(StableRNG(42), subj_n, item_n,
+					 subj_btwn = subj_btwn,
+					 item_btwn = item_btwn,
                      both_win = both_win)
 form = @formula(dv ~ 1 + age + pet + cond + time  + (1|subj) + (1|item));
 cont = Dict(nm => HelmertCoding() for nm in (:age, :pet, :cond, :time));
 fm1 = fit(MixedModel, form, dat, contrasts=cont, REML=false);
 zpmt = simulate_waldtests(StableRNG(42),10,fm1,use_threads=false);
+bsamp = parametricbootstrap(StableRNG(42),10,fm1,use_threads=false);
+
 
 @testset "sim_to_df" begin
     df = sim_to_df(zpmt)
@@ -35,5 +37,5 @@ end
     @test ncol(pt) == 2
     @test names(pt) == ["coefname", "power"]
     @test string.(pt.coefname) == collect(string.(zpmt[1][1] |> keys))
-    @test pt.power == [0.4, 0.1, 0.2, 0.2, 0.1]
+    @test pt.power == [0.3, 0.2, 0.1, 0.0, 0.3]
 end
