b2a578f3b0ed3ccc36698e0d06ded2fbb12d43e8
Phillip Alday <palday@users.noreply.github.com>
2024-02-16 03:04:43 +0000

    Update src/utilities.jl

--- src/utilities.jl
+++ src/utilities.jl
@@ -281,7 +281,7 @@ random effects.
     [`update!`](@ref) requires.
 """
 function create_re(sigmas...; corrmat=nothing)
-    @assert all(>(0),sigmas) "all σs must be positive"
+    all(>=(0), sigmas) || throw(ArgumentError("all σs must be positive"))
     ss = Diagonal([sigmas...])
 
     isnothing(corrmat) ? LowerTriangular(ss) : ss * cholesky(Symmetric(corrmat, :L)).L
