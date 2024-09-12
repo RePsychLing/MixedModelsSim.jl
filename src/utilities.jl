76ec8cec61f46287cdd6704b78e5c41debcd48eb
Phillip Alday <me@phillipalday.com>
2024-09-12 11:55:16 +0000

    note about sigmas being relative

--- src/utilities.jl
+++ src/utilities.jl
@@ -273,6 +273,11 @@ Create the covariance factor for a random effect from the standard deviations an
 The `sigmas` should be specified in the same order as the random slopes in the
 output of `VarCorr(m)`.
 
+!!! note
+    The `sigmas` are specified **relative** to the residual standard deviation. 
+    Absolute values must be scaled by dividing by the assumed residual 
+    standard deviation.
+
 The correlation matrix defaults to the identity matrix, i.e. no correlation between
 random effects.
 
