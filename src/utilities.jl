2685b7fd48cfcb97139591ff5771cc33d7beba1f
Phillip Alday <palday@users.noreply.github.com>
2024-09-12 06:02:18 -0700

    Doc improvements (#76)
    * note about sigmas being relative
    
    * version bump
    
    * add note to update!
    
    * docs compat

--- src/utilities.jl
+++ src/utilities.jl
@@ -216,6 +216,11 @@ The `namedre` can be created using [`create_re`](@ref). The `namedre` are specif
 by the name of the blocking variable, e.g. `subj=create_re(...)`.
 
 !!! warning
+    The model's response must be initialized to a non-constant value before
+    calling this function, otherwise the model update will fail with a 
+    `PosDefException`.
+
+!!! warning
     Setting θ directly as a keyword-argument is deprecated.
 
 !!! note
@@ -272,6 +277,11 @@ Create the covariance factor for a random effect from the standard deviations an
 
 The `sigmas` should be specified in the same order as the random slopes in the
 output of `VarCorr(m)`.
+
+!!! note
+    The `sigmas` are specified **relative** to the residual standard deviation. 
+    Absolute values must be scaled by dividing by the assumed residual 
+    standard deviation.
 
 The correlation matrix defaults to the identity matrix, i.e. no correlation between
 random effects.
